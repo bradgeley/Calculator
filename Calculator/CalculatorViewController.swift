42e174441f7c8e2fbee678e1b116615169fc4029
bradjc95certitude <bradjc95@gmail.com>
2019-05-16 22:53:31 -0500

    Test Commit

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -9,12 +9,95 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
+    struct Constants {
+        
+        static let OutputFont = "Helvetica"
+        static let OutputFontSizeLandscape: CGFloat = 50.0
+        static let OutputFontSizePortrait: CGFloat = 30
+        
+        static let ButtonFont = "Helvetica"
+        static let ButtonFontSizeLandscape: CGFloat = 35
+        static let ButtonFontSizePortrait: CGFloat = 30
+        
+    }
+    
+    
+/* IBOutlets */
+    
+    @IBOutlet var LabelsToFormat: [UILabel]!
+    @IBOutlet var ButtonsToFormat: [UIButton]!
+    @IBOutlet weak var OutputLabel: UILabel!
+    @IBOutlet weak var InputLabel: UILabel!
+    @IBOutlet weak var LogView: CalculatorLogView!
+    
+    
+/* Variables */
+    
+    var output: String? {
+        didSet {
+            OutputLabel.text = output
+            formatLabels()
+        }
+    }
+    
+    var input: String? {
+        didSet {
+            InputLabel.text = input
+            formatLabels()
+        }
+    }
+    
+    var outputLabelFont: UIFont? {
+        return ((UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft
+              || UIDevice.current.orientation == UIDeviceOrientation.landscapeRight) ?
+              UIFont(name: Constants.OutputFont, size: Constants.OutputFontSizeLandscape)
+            : UIFont(name: Constants.OutputFont, size: Constants.OutputFontSizePortrait))
+    }
+    
+    var buttonFont: UIFont? {
+        return ((UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft
+            || UIDevice.current.orientation == UIDeviceOrientation.landscapeRight) ?
+                UIFont(name: Constants.ButtonFont, size: Constants.ButtonFontSizeLandscape)
+            : UIFont(name: Constants.ButtonFont, size: Constants.ButtonFontSizePortrait))
+    }
+    
+    
+/* Private Functions */
+    
+    private func formatAllText() {
+        formatButtons()
+        formatLabels()
+    }
+    
+    private func formatButtons() {
+        for button in ButtonsToFormat {
+            let scaledFont = UIFontMetrics(forTextStyle: .headline).scaledFont(for: outputLabelFont!)
+            button.titleLabel?.font = scaledFont
+        }
+    }
+    
+    private func formatLabels() {
+        for label in LabelsToFormat {
+            let scaledFont = UIFontMetrics(forTextStyle: .headline).scaledFont(for: outputLabelFont!)
+            label.font = scaledFont
+        }
+    }
+    
+    
+/* Override functions */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //formatAllText()
     }
-
-
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        //formatAllText()
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        //formatAllText()
+    }
 }
 
