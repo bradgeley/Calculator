c74023069ffa3953ee56638f4fd3489ead2bf37d
bradjc95certitude <bradjc95@gmail.com>
2019-05-16 22:53:08 -0500

    first commit

---
+++ Calculator/Calculator.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Bradley Christensen on 5/15/19.
+//  Copyright © 2019 Bradley Christensen. All rights reserved.
+//
+
+import Foundation
---
+++ Calculator/CalculatorLogView.swift
@@ -0,0 +1,11 @@
+//
+//  LogView.swift
+//  Calculator
+//
+//  Created by Bradley Christensen on 5/16/19.
+//  Copyright © 2019 Bradley Christensen. All rights reserved.
+//
+
+import Foundation
+
+class 
